12ec74d2fd657b07636eb582f98c351cf77cab00
Siva Sivakumar <siva.sivakumar@nokia.com>
2024-11-23 17:57:06 +0100

    draft 16 docker build

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ BASEDIR=$(realpath $(dirname "$0"))
 PROXY_BUILD_ARGS="--build-arg http_proxy=${http_proxy} --build-arg HTTP_PROXY=${HTTP_PROXY} --build-arg https_proxy=${https_proxy} --build-arg HTTPS_PROXY=${HTTPS_PROXY}"
 
 # build the automation panel container
-docker build ${PROXY_BUILD_ARGS} ${BASEDIR}
+docker build ${PROXY_BUILD_ARGS} -t siva19susi/generic-yang-browser ${BASEDIR}
