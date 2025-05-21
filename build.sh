c11aae3c1d7aa94e07bd7f29cfc31e8924eefc68
Siva Sivakumar <siva.sivakumar@nokia.com>
2025-05-21 18:38:50 +0200

    update

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ PLATFORM_ARGS="--platform linux/amd64,linux/arm64"
 PROXY_BUILD_ARGS="--build-arg http_proxy=${http_proxy} --build-arg HTTP_PROXY=${HTTP_PROXY} --build-arg https_proxy=${https_proxy} --build-arg HTTPS_PROXY=${HTTPS_PROXY}"
 
 # build the automation panel container
-docker build ${PROXY_BUILD_ARGS} ${PLATFORM_ARGS} -t sivasusi19/nsp-yang-browser:srx ${BASEDIR}
+docker build ${PROXY_BUILD_ARGS} ${PLATFORM_ARGS} -t sivasusi19/nsp-yang-browser:latest ${BASEDIR}
